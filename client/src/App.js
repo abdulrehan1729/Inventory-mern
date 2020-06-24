705030e3245253ad91a9f31e053588ca16de2793
Abdul Rehan <abdulrehan1729@gmail.com>
2020-06-25 01:33:09 +0530

    interim

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
@@ -6,16 +6,18 @@ import Inventory from "./components/Inventory";
 import Navbar from "./components/Navbar";
 import Billing from "./components/Billing";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Route exact path="/" render={() => <Inventory />} />
-        <Route path="/billing" render={() => <Billing />} />
-      </Router>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Route exact path="/" render={() => <Inventory />} />
+          <Route path="/billing" render={() => <Billing />} />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
