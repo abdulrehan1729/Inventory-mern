6fbf6c5ab205104247d97e129d468b10f60a2e7b
Abdul Rehan <abdulrehan1729@gmail.com>
2020-06-25 01:42:59 +0530

    deployment settings

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const routes = require('./routers/routes.js')
-require('./db/index')
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const path = require("path");
+const routes = require("./routers/routes.js");
+require("./db/index");
 
-const port = process.env.PORT || 8009
+const port = process.env.PORT || 8009;
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-app.use(bodyParser.json())
+app.use("/", express.static(path.join(__dirname, "/client/build")));
 
-app.use('/', routes)
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
+
+app.use("/", routes);
 
 app.listen(port, () => {
-    console.log(`Server is running on ${port}`)
-})
+  console.log(`Server is running on ${port}`);
+});
